4cbacb2e108a8336b9eaddaf8ea49f0dbbaa4f4b
Naumov Ilya <naumovilya99@mail.ru>
2021-03-09 20:01:19 +0300

    edit task_17.10.1

--- task_17.10.1.php
+++ task_17.10.1.php
@@ -1,37 +1,60 @@
 <?php
 
-class family
+class Family
 {
-	protected $peopleCount = 3;
+	protected $peopleCount = 0;
 	protected $surname = 'Ivanov';
 }
 
 
-class father extends family
+class Father extends Family
 {
-	protected $age  = 30;
+	protected $age  = 0;
 	protected $wife = ''; 
 	protected $name = '';
-
-}
-
-class mother extends family
-{
-	protected $age  = 30; 
-	protected $husband = '';
-	protected $name = '';
-
-}
-
-class child extends family
-{
-	
-	function __construct()
+	// когда создаём новый объект, количество членов семьи увеличивается
+	// при создании объекта необходимо указать имя, жену, возраст
+	function __construct(string $name, Mother $wife, int $age)
 	{
 		++$this->peopleCount;
+		$this->name = $name;
+		$this->$age = $age;
+		$this->wife = $wife;
 	}
 
-	protected $mother = '';
-	protected $father = '';
+}
+
+class Mother extends Family
+{
+	protected $husband = '';
+	protected $name = '';
+	protected $age  = 0; 
+	// когда создаём новый объект, количество членов семьи увеличивается
+	// при создании объекта необходимо указать имя, мужа, возраст
+	function __construct(string $name, Father $husband, int $age)
+	{
+		++$this->peopleCount;
+		$this->name = $name;
+		$this->$age = $age;
+		$this->husband = $husband;
+	}
+
+}
+
+class Child extends Family
+{
+	protected $name = '';
+	protected $mother;
+	protected $father;
+	// когда создаём новый объект, количество членов семьи увеличивается
+	// при создании объекта child необходимо указать имя, пол (0 - девочка, 1 - мальчик), объекты родителей
+	function __construct(string $name, bool $isMale, Father $father, Mother $mother)
+	{
+		++$this->peopleCount;
+		$this->name = $name;
+		$this->mother = $mother;
+		$this->father = $father;
+	}
+
 }
 ?>
